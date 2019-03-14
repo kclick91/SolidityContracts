e457be2885127040b9a5da5f98e62013aafba04f
Kelly Click <kclick91@gmail.com>
2019-03-14 14:22:57 -0400

    Create PasswordContractRestricted.sol

---
+++ PasswordContractRestricted.sol
@@ -0,0 +1,95 @@
+pragma solidity >=0.4.22 <0.6.0;
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address private _owner;
+
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() internal {
+    _owner = msg.sender;
+    emit OwnershipTransferred(address(0), _owner);
+  }
+
+  /**
+   * @return the address of the owner.
+   */
+  function owner() public view returns(address) {
+    return _owner;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(isOwner());
+    _;
+  }
+
+  /**
+   * @return true if `msg.sender` is the owner of the contract.
+   */
+  function isOwner() public view returns(bool) {
+    return msg.sender == _owner;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   * @notice Renouncing to ownership will leave the contract without an owner.
+   * It will not be possible to call the functions with the `onlyOwner`
+   * modifier anymore.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipTransferred(_owner, address(0));
+    _owner = address(0);
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    _transferOwnership(newOwner);
+  }
+
+  /**
+   * @dev Transfers control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function _transferOwnership(address newOwner) internal {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(_owner, newOwner);
+    _owner = newOwner;
+  }
+}
+contract PasswordContract is Ownable {
+    
+    event MsgEvent(string msg);
+    bytes32 b = 0xad27c76148d1b3fffbc840d58c1c4ad9c4b8372e4e97bbd3cad9f0da7f9a8d00;
+    //Enter correct password and the message to create an event
+    function pwdFunction(string memory pwd,string memory msg, bytes32 newpwd) public onlyOwner
+    {
+        require (keccak256(abi.encodePacked(pwd)) == b);
+         emit MsgEvent(msg);
+         b = newpwd;
+    }
+    
+    //Enter a password with keccak256 hash, include 0x
+    function setPassword(bytes32 pwd, bytes32 oldpwd) public onlyOwner
+    {
+        require(keccak256(abi.encodePacked(oldpwd)) == b);
+        b = pwd;
+    }
+    
+}
